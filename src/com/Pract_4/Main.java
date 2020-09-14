96e866ccc38e0d9945206fb90542807f4d76a1aa
TheDavidGame <vlad.billI@bk.ru>
2020-09-15 00:20:45 +0300

    ex4_2
    finish

--- src/com/Pract_4/Main.java
+++ src/com/Pract_4/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+        Author c1 = new Author("Alex","Alex@gmail.ru",'m');
+        System.out.println(c1.getEmail());
+        System.out.println(c1.getName());
+        System.out.println(c1.toString());
     }
 }
