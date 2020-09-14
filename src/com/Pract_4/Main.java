1fdb8d52a7a1c8f3f54a8d0bfb5fc911172359b5
TheDavidGame <vlad.billI@bk.ru>
2020-09-14 23:24:05 +0300

    Initial commit

---
+++ src/com/Pract_4/Main.java
@@ -0,0 +1,9 @@
+package com.Pract_4;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
